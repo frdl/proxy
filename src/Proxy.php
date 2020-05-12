0dd2b1ba83659fa48524addcf6399ceb14785669
Till Wehowski <github@webfan.de>
2020-05-12 21:50:09 +0200

    Update Proxy.php

--- src/Proxy.php
+++ src/Proxy.php
@@ -34,7 +34,7 @@ class Proxy
 	
 	public function __construct(string $deploy = null, string $targetLocation = null, string $targetSeverHost = null, string $httpHost = null, string $method = null, $protocol = null, bool $HostHeaderOverwrite = null){
 
-                (new \GuzzleHttp\LoadGuzzleFunctionsForFrdl());
+          
 		
 		$this->targetSeverHost = $targetSeverHost ? $targetSeverHost : $_SERVER['SERVER_NAME'];
 		$this->httpHost = $httpHost ? $httpHost : $_SERVER['HTTP_HOST'];
